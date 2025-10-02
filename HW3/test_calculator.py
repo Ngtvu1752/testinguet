66288a7cd87560787f7a783e71aaea5134b34dda
Ngtvu1752 <23021752@vnu.edu.vn>
2025-10-02 15:43:05 +0700

    HW3

--- HW3/test_calculator.py
+++ HW3/test_calculator.py
@@ -1,44 +1,47 @@
 import pytest
 from library_calculator import calculate_discount
 
-test_data = [
+decision_table_test_data = [
     (-1, 8, "Invalid"),
     (21, 8, "Invalid"),
     (7, 0, "Invalid"),
     (7, 13, "Invalid"),
-    (2, 3, "0% discount"),
-    (3, 8, "5% discount"),
-    (7, 4, "10% discount"),
-    (8, 10, "15% discount"),
-    (15, 5, "20% discount"),
-    (12, 9, "25% discount"),
 
-
-    (7, 1, "10% discount"),      # nom-min1
-    (7, 6, "15% discount"),      # nom-max1
-    (7, 12, "15% discount"),     # nom-max2
-    (0, 8, "5% discount"),       # min1-nom
-    (5, 8, "15% discount"),      # max1-nom
-
-    (10, 8, "15% discount"),     # max2-nom
-    
-    (20, 8, "25% discount"),      # max3-nom
-    (7, 8, "15% discount"),       # nom-nom
-
-    (7, 2, "10% discount"),
-    (7, 5, "10% discount"),
-    (7, 7, "15% discount"),
-    (7, 11, "15% discount"),
-    (1, 8, "5% discount"),
-    (4, 8, "5% discount"),
-    (6, 8, "15% discount"),
-    (9, 8, "15% discount"),
+    (2, 3, "0% discount"),      # Rule: sSMTB < 5, tGGH < 6
+    (3, 8, "5% discount"),      # Rule: sSMTB < 5, tGGH >= 6
+    (7, 4, "10% discount"),     # Rule: 5 <= sSMTB <= 10, tGGH < 6
+    (8, 10, "15% discount"),    # Rule: 5 <= sSMTB <= 10, tGGH >= 6
+    (15, 5, "20% discount"),    # Rule: sSMTB > 10, tGGH < 6
+    (12, 9, "25% discount"),    # Rule: sSMTB > 10, tGGH >= 6
 ]
 
-@pytest.mark.parametrize("sSMTB, tGGH, expected_output", test_data)
+
+bva_test_data = [
+    (0, 8, "5% discount"),      # Biên dưới của sSMTB
+    (20, 8, "25% discount"),     # Biên trên của sSMTB
+    (7, 1, "10% discount"),      # Biên dưới của tGGH
+    (7, 12, "15% discount"),     # Biên trên của tGGH
+    (5, 8, "15% discount"),      # Biên giữa của sSMTB
+    (10, 8, "15% discount"),     # Biên giữa của sSMTB
+    (7, 6, "15% discount"),      # Biên giữa của tGGH
+    
+    (1, 8, "5% discount"),       # Cận biên của 0
+    (4, 8, "5% discount"),       # Cận biên của 5
+    (6, 8, "15% discount"),      # Cận biên của 5
+    (9, 8, "15% discount"),      # Cận biên của 10
+    (7, 2, "10% discount"),      # Cận biên của 1
+    (7, 5, "10% discount"),      # Cận biên của 6
+    (7, 7, "15% discount"),      # Cận biên của 6
+    (7, 11, "15% discount"),     # Cận biên của 12
+    
+    (7, 8, "15% discount"),
+]
+
+all_test_data = decision_table_test_data + bva_test_data
+
+@pytest.mark.parametrize("sSMTB, tGGH, expected_output", all_test_data)
 def test_comprehensive_cases(sSMTB, tGGH, expected_output):
-    """
-    """
+
     actual_output = calculate_discount(sSMTB, tGGH)
     
     assert actual_output == expected_output
